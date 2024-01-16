d8476af268e7358bdae62ee596dd8967508ae489
gong5518 <gong5518@naver.com>
2024-01-16 10:42:22 +0900

    setting

---
+++ build/generated/sources/annotationProcessor/java/main/kr/co/seoulit/erp/account/operate/system/mapstruct/BoardReqMapStructImpl.java
@@ -0,0 +1,50 @@
+package kr.co.seoulit.erp.account.operate.system.mapstruct;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import kr.co.seoulit.erp.account.operate.system.entity.BoardEntity;
+import kr.co.seoulit.erp.account.operate.system.to.BoardReqDTO;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-16T10:38:48+0900",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.4.1.jar, environment: Java 11.0.11 (Oracle Corporation)"
+)
+@Component
+public class BoardReqMapStructImpl implements BoardReqMapStruct {
+
+    @Override
+    public BoardEntity toEntity(BoardReqDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BoardEntity boardEntity = new BoardEntity();
+
+        boardEntity.setStatus( dto.getStatus() );
+        boardEntity.setId( dto.getId() );
+        boardEntity.setTitle( dto.getTitle() );
+        boardEntity.setContents( dto.getContents() );
+        boardEntity.setWrittenBy( dto.getWrittenBy() );
+        boardEntity.setWriteDate( dto.getWriteDate() );
+        boardEntity.setUpdateDateTime( dto.getUpdateDateTime() );
+
+        return boardEntity;
+    }
+
+    @Override
+    public List<BoardEntity> toEntity(List<BoardReqDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<BoardEntity> list = new ArrayList<BoardEntity>( dtos.size() );
+        for ( BoardReqDTO boardReqDTO : dtos ) {
+            list.add( toEntity( boardReqDTO ) );
+        }
+
+        return list;
+    }
+}
